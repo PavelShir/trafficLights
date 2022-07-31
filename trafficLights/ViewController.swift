57b7c897f2bb45ddfaa8a2bce9f197a3acc7cd15
Pavel Shirokiy <pshirokiy@gmail.com>
2022-07-31 20:25:28 +0300

    part 1 done

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLihgt: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var lightControllerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        yellowLihgt.layer.cornerRadius = yellowLihgt.frame.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+        
+       // lightControllerButton.currentTitle = ""
         
     }
 
-
+    @IBAction func shangeColor() {
+        redLight.alpha = 1
+        lightControllerButton.setTitle("Next", for: .normal)
+    }
+    
 }
 
